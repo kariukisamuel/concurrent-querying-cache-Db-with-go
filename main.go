2fea17688f543815159d05affb9a5d563a3c4dd3
samuel <inboxsamuel@outlook.com>
2022-02-11 13:37:46 +0300

    Added Mutexes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,31 +11,43 @@ var cache = map[int]Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
+	wg := &sync.WaitGroup{}
+	m := &sync.RWMutex{}
+
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		if b, ok := queryCache(id); ok {
-			fmt.Println("From Cache:")
-			fmt.Println(b)
-			continue
-		}
-		if b, ok := queryDatabase(id); ok {
-			fmt.Println(b)
-			continue
-		}
-		fmt.Printf("Book not found with id: %v", id)
-		time.Sleep(time.Second)
+		wg.Add(2)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+			if b, ok := queryCache(id, m); ok {
+				fmt.Println("From Cache:")
+				fmt.Println(b)
+			}
+		}(id, wg, m)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+			if b, ok := queryDatabase(id, m); ok {
+				fmt.Println("From Database:")
+				fmt.Println(b)
+			}
+		}(id, wg, m)
+		time.Sleep(150 * time.Millisecond)
 	}
+	wg.Wait()
+
 }
 
-func queryCache(id int) (Book, bool) {
+func queryCache(id int, m *sync.RWMutex) (Book, bool) {
+	m.RLock()
 	b, ok := cache[id]
+	m.RUnlock()
 	return b, ok
 }
-func queryDatabase(id int) (Book, bool) {
+func queryDatabase(id int, m *sync.RWMutex) (Book, bool) {
 	time.Sleep(time.Millisecond * 100)
 	for _, b := range books {
 		if b.ID == id {
+			m.Lock()
 			cache[id] = b
+			m.Unlock()
 			return b, true
 		}
 	}
